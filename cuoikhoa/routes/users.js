9c430f6a6abb5c12a7a1dd57341e035536d8b298
MSI\trung <bin.trung0@gmail.com>
2023-03-28 20:19:26 +0700

    test thôi được ko được thì thôi

--- cuoikhoa/routes/users.js
+++ cuoikhoa/routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const {
+  registerUser,
+  authLogin,
+  getUserProfile,
+  getAllUser,
+  updateUserProfile,
+} = require("../controllers/useControllers");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// dk user
+router.post("/", registerUser);
+//login
+router.post("/login", authLogin);
+//cá nhân
+router.get("/profile",  getUserProfile);
+// sửa cá nhân
+router.put("/profile",  updateUserProfile);
 
 module.exports = router;
